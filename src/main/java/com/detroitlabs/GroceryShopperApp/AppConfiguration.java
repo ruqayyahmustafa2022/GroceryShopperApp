941514d4b621fb03bf196b17cac864d40cf33f75
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-04-20 15:53:06 -0400

    HTML pages for each view and stencil

---
+++ src/main/java/com/detroitlabs/GroceryShopperApp/AppConfiguration.java
@@ -0,0 +1,15 @@
+package com.detroitlabs.GroceryShopperApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+@EnableAutoConfiguration
+@ComponentScan
+public class AppConfiguration {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppConfiguration.class, args);
+
+    }
+
+}
